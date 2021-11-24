32d5fbb4a4a3159a1485774cb5b87a94c21b5742
ryandhikabaa <ryandhikabintang0@gmail.com>
2021-11-24 13:13:55 +0700

    [PERTEMUAN] Materi Looping

---
+++ Materi/pertemuan14_or8.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    //Contoh While
+    int a = 10,n,i;
+
+    // while (a < 20)
+    // {
+    //     cout << "value of a : " << a << endl;
+    //     a++;
+    // }
+
+    //Contoh do while
+    // do
+    // {
+    //     cout << "value of a : " << a << endl;
+    //     a++;
+    // } while (a < 20);
+
+    //For
+    // for (a = 10; a < 20; a++)
+    // {
+    //     cout << "value of a : " << a << endl;
+    // }
+
+    //forever end with condition
+    // while (true)
+    // {
+    //     cout << "value of a : " << a << endl;
+    //     a++;
+    //     if (a > 19)
+    //     {
+    //         break;
+    //     }
+        
+    // }
+    
+    // For with dicrement
+    // cin >> n;
+    // for (;n>0;n--)
+    // {
+    //     cout << "mahasiswa " << endl;
+    // }
+
+    //While with dicrement
+    // cin >> n;
+    // while (n--)
+    // {
+    //     cout << "mahasiswa " << n << endl;
+    // }
+    
+    // i = 1;
+    // cin >> n;
+    // for (;n>0;n--)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // i = 1;
+    // cin >> n;
+    // for (i=0;i < n ;i++)
+    // {
+    //     cout << i + 1 << endl;
+    // }
+    
+
+    //versi singkat
+    // cin >> n;
+    // for (i = 1; i >0; n--, i++)
+    // {
+    //     cout << i << endl;
+    // }
+    
+    // chatbotsederhana
+
+    // char belokkiri;
+
+    // do{
+    //     cout << "apa mau belok kiri ? (y/n)";
+    // }while();
+    
+
+    return 0;
+}
--- Materi/tempCodeRunnerFile.cpp
+++ Materi/tempCodeRunnerFile.cpp
@@ -1,1 +1,6 @@
-a++
+int i;
+    int j = 1;
+    for (int i = 1; i<=5 ; i++, j=i*i)
+    {
+        cout << j << endl;
+    }
